0343ff84fca5e16cbbf950c345f2fa49f004dd3a
Alex <olivesargreek@gmail.com>
2022-04-27 16:24:42 +0300

    fix: stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,6 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
   push(element) {
     this.stack.push(element);
     // remove line with error and write your code here
